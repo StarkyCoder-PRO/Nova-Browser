65fdb94e1a5ffd095493e799b66a59dd11badc03
Karl <atomica.c.perry@atomicmail.io>
2026-01-06 16:56:55 -0500

    Enhance start script with sleep and Edge launch
    Added sleep command for stabilization and launched Microsoft Edge.

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -8,4 +8,10 @@ export DISPLAY=:1
 x11vnc -display :1 -nopw -forever -shared &
 
 # Start noVNC
-websockify --web=/usr/share/novnc/ 6080 localhost:5900
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
+
+# Wait a few seconds for desktop to stabilize
+sleep 5
+
+# Launch Microsoft Edge
+microsoft-edge-stable &
